076a49c8014127e7c95065e08dfb4d583c24946e
ItsJustVaal <109011972+ItsJustVaal@users.noreply.github.com>
2023-01-25 18:03:56 -0700

    added comment

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ import include
 connection = include.help.create_server_connection()
 
 include.db.insert_data(connection)
+
+
+# This file is the last thing to flesh out completely since it just runs the program
